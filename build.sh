f16a67515185fbc68d372c00dc3a1974820dafb8
elektroencefalografista <30481447+elektroencefalografista@users.noreply.github.com>
2023-06-04 00:25:17 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 docker build --build-arg userid=$(id -u) --build-arg groupid=$(id -g) --build-arg username=$(id -un) -t coreboot-build .
-
-# docker run -it --rm -v $ANDROID_BUILD_TOP:/src android-build-trusty
-# cd /src; source build/envsetup.sh
-# lunch aosp_arm-eng
-# m -j50
