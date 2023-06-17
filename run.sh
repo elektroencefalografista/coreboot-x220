566282ce908f57616f2ebecb6c70b748fc03dd36
elektroencefalografista <30481447+elektroencefalografista@users.noreply.github.com>
2023-06-17 12:53:11 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# you might need to mount /src somewhere if you dont want to redownload the coreboot repo each time, ie. -v /your/dir:src
-docker run -it --rm coreboot-build
+mkdir src
+docker run -it --rm -v $(pwd)/src:/src coreboot-build
